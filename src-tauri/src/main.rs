d4c9e55228c8f0cb1207f94bcbd1caabaf76dd61
= <doublenegative@protonmail.com>
2022-11-20 21:25:01 +0100

    logging plugin. removed greet command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,12 +18,6 @@ use state::navigation::{
 use tauri_plugin_log::{LogTarget, LoggerBuilder};
 use ui::menu::{create_menu, handle_menu_event};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    println!("cake");
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
     tauri::Builder::default()
